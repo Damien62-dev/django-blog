5ac0ca56d75487b776c8017e8021b1fc2a576759
Damien Mullet <damien.mullet@gmail.com>
2026-03-05 19:35:07 +0000

    fix ALLOWED_HOSTS

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-!_65l+s$c$l7eth&gwi^fa(3+b@nik*@k=hy9^q_stetj28t%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('RENDER_EXTERNAL_HOSTNAME', '')]
 
 
 # Application definition
